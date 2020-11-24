6bbb40f3c9c305b2c84adb9d19cddf4b3b93c8b2
django <mrdjangodev@gmail.com>
2020-11-25 00:52:04 +0200

    minor refactor

--- shell.c
+++ shell.c
@@ -4,31 +4,43 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include "h.h"
+// #include "h.h"
 
 //move to header file
 #define MAX_LINE 80 /* The maximum length command */
 
 int _BG = 0;
 
+// I/O routines to parse and print the argument vector
 void parse_input(char *in, char *argv[]);
+void print_argv(char *argv[MAX_LINE]);
+// void sarrcpy(char *last[MAX_LINE], char *argv[]);
 
 int main(void)
 {
+    //input buffer
     char in[MAX_LINE];
-    char *argv[MAX_LINE];
+    char *argv[MAX_LINE] = {0};
+    char *last[MAX_LINE] = {0};
+
     int __RUN = 1;
     while (__RUN)
     {
-        printf("Hshell>> ");
+        printf("Hsh~>> ");
         fflush(stdout);
         parse_input(in, argv);
-        if(!strcmp(argv[0] ,"exit()" )) exit(0);
         pid_t pid = fork();
-        if (pid == 0)
+        //fork failed!
+        if (pid < 0)
         {
+            fprintf(stderr, "Failed Fork");
+            return 1;
+        }
+        else if (pid == 0)
+        {
+            // sarrcpy(argv, last);
             int ret = execvp(argv[0], argv); // Run argv[0] with argv[1..] flagss
-            //set flag to default 
+            //set foreground
             _BG = 0;
             if (ret == -1)
                 fprintf(stderr, "%s: command not found\n", argv[0]);
@@ -46,13 +58,21 @@ int main(void)
 
 void parse_input(char *in, char *argv[MAX_LINE])
 {
-    //strtok modifies stirng 
+    //strtok modifies stirng
     char *tmp = in;
     scanf("%80[^\n]", in);
     getchar();
     int _CNT = 0;
     // Extract the first token
     char *token = strtok(in, " ");
+    if (!strcmp(token, "exit()"))
+        exit(0);
+    //stop parsing and return 
+    if (!strcmp(token, "!!")){
+        // sarrcpy(argv, last);
+        return;
+    } 
+
     // loop through the string to extract all other tokens
     while (token != NULL)
     {
@@ -68,4 +88,17 @@ void parse_input(char *in, char *argv[MAX_LINE])
             //setting background flag to true
             _BG = 1;
         }
+}
+
+void print_argv(char *argv[MAX_LINE])
+{
+    for (size_t i = 0; i < MAX_LINE - 1; i++){
+
+        if (argv[i])
+            printf("%s ", argv[i]);
+        else {
+            putchar('\n');
+            break;
+        }
+    }
 }
